28ac0f8292b8da898dd42484fbfd655319e9651b
DESKTOP-K0TE4AO\sebas <sebastiangarciapachano@gmail.com>
2024-05-05 17:39:25 +0200

    refactoring

--- app/src/main/java/com/example/sprint2/AreaCalculatorViewModel.kt
+++ app/src/main/java/com/example/sprint2/AreaCalculatorViewModel.kt
@@ -4,12 +4,13 @@ import androidx.lifecycle.ViewModel
 
 class AreaCalculatorViewModel: ViewModel() {
 
-    fun getCircle(radius: String): Double{
-        val area = radius.toDouble()
+    fun getCircle(radius: String): Int{
+        val area = radius.toInt()
         val pi = 3.14
         val result = pi * area * area
+        val finalResult = result.toInt()
 
-        return result
+        return finalResult
     }
     fun getSquare(base: String, height: String): Int {
         val result = base.toInt() * height.toInt()
@@ -18,6 +19,29 @@ class AreaCalculatorViewModel: ViewModel() {
     }
     fun getTriangle(base: String, height: String) :Int {
         val result = (base.toInt() * height.toInt()) /2
+
+        return result
+    }
+
+    fun shape(shape: String, base: String, height: String): Int {
+        var result = 0
+            when (shape) {
+                "circle" -> {
+                    if(base.isNotEmpty()){
+                        result = getCircle(base)
+                    }
+                }
+                "square" -> {
+                    if(base.isNotEmpty() && height.isNotEmpty()){
+                        result = getSquare(base, height)
+                    }
+                }
+                "triangle" -> {
+                    if(base.isNotEmpty() && height.isNotEmpty()){
+                        result = getTriangle(base, height)
+                    }
+                }
+            }
 
         return result
     }
