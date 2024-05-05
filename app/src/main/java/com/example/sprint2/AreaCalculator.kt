f2236742934e44f96e4046c13f7fcb0604086925
DESKTOP-K0TE4AO\sebas <sebastiangarciapachano@gmail.com>
2024-05-05 11:56:34 +0200

    circle area

--- app/src/main/java/com/example/sprint2/AreaCalculator.kt
+++ app/src/main/java/com/example/sprint2/AreaCalculator.kt
@@ -2,6 +2,7 @@ package com.example.sprint2
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -9,7 +10,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.sprint2.databinding.ActivityAreaCalculatorBinding
 
-class AreaCalculator : AppCompatActivity(){
+class AreaCalculator : AppCompatActivity(), View.OnClickListener{
 
     private val areaCalculatorViewModel: AreaCalculatorViewModel by viewModels()
     private lateinit var binding: ActivityAreaCalculatorBinding
@@ -37,14 +38,8 @@ class AreaCalculator : AppCompatActivity(){
                     binding.tvBase.text = "Radius"
                     binding.tvHeight.visibility = View.INVISIBLE
                     binding.etHeight.visibility = View.INVISIBLE
-                    areaCalculatorViewModel.getCircle()
                 }
-                "square" -> {
-                    areaCalculatorViewModel.getSquare()
-                }
-                "triangle" -> {
-                    areaCalculatorViewModel.getTriangle()
-                }
+                
 
             }
         }
@@ -52,6 +47,19 @@ class AreaCalculator : AppCompatActivity(){
 
     private fun configClickListener() {
         binding.btCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        if(binding.tvBase.text == "Radius"){
+            if(binding.etBase.text.isNotEmpty()){
+                val radius = binding.etBase.text.toString()
+                val result = areaCalculatorViewModel.getCircle(radius)
+                binding.tvResult.text = "El área del círculo es $result"
+            } else{
+                Toast.makeText(this, "Pon un número", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 
 
--- app/src/main/java/com/example/sprint2/AreaCalculatorViewModel.kt
+++ app/src/main/java/com/example/sprint2/AreaCalculatorViewModel.kt
@@ -3,4 +3,12 @@ package com.example.sprint2
 import androidx.lifecycle.ViewModel
 
 class AreaCalculatorViewModel: ViewModel() {
+
+    fun getCircle(radius: String): Double{
+        val area = radius.toDouble()
+        val pi = 3.14
+        val result = pi * area * area
+        return result
+    }
+
 }
